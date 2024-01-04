39845c9a16f25130833024707f1108f95f5a5d14
मयंक <126972040+MayankDevil@users.noreply.github.com>
2024-01-04 16:22:47 +0000

    Update js/

--- js/script.js
+++ js/script.js
@@ -11,52 +11,65 @@ try
         | MENU FUNCTION | onclick add else remove menu
         ======================================================
     */ 
-    let menu_btn = document.getElementById('menu_btn');
+    // let menu_btn = document.getElementById('menu_btn');
 
-    let menu = document.getElementById('menu');
+    // let menu = document.getElementById('menu');
 
-    let nav = document.getElementById('nav');
+    // let nav = document.getElementById('nav');
 
-    var n = 0;
+    // var n = 0;
     
-    menu_btn.onclick = function()
-    {
-        if(n == 0)
-        {
-            this.innerHTML = "&#8942;";
-            menu.style.display = "grid";
-            nav.classList.add('nav_open')
-            n = 1;
-        }
-        else
-        {
-            this.innerHTML = "&#9776;";
-            menu.style.display = "";
-            nav.classList.remove('nav_open')
-            n = 0;
-        }
-    }
+    // menu_btn.onclick = function()
+    // {
+    //     if(n == 0)
+    //     {
+    //         this.innerHTML = "&#8942;";
+    //         menu.style.display = "grid";
+    //         nav.classList.add('nav_open')
+    //         n = 1;
+    //     }
+    //     else
+    //     {
+    //         this.innerHTML = "&#9776;";
+    //         menu.style.display = "";
+    //         nav.classList.remove('nav_open')
+    //         n = 0;
+    //     }
+    // }
     /*
         -------------------------------------------------------
          anchor function  : onclikc anchor close menu
         -------------------------------------------------------
     */ 
-    let links = document.querySelectorAll("#menu a");
+    // let links = document.querySelectorAll("#menu a");
 
-    for(let i = 0; i < links.length; i++)
-    {    
-        links[i].onclick = function()
-        {
-            menu_btn.innerHTML = "&#9776;";
-            menu.style.display = "";
-            nav.classList.remove('nav_open')
-            n = 0;
-        }
-    }
+    // for(let i = 0; i < links.length; i++)
+    // {    
+    //     links[i].onclick = function()
+    //     {
+    //         menu_btn.innerHTML = "&#9776;";
+    //         menu.style.display = "";
+    //         nav.classList.remove('nav_open')
+    //         n = 0;
+    //     }
+    // }
     /*
         ========================================================
-    */ 
-    
+    */
+
+    let scrl_up = document.getElementById('scrl_up')
+
+    window.addEventListener('scroll',function()
+    {
+        if (window.scrollY < 150)
+        {
+            scrl_up.style.display = `none`
+        }
+        else
+        {
+            scrl_up.style.display = `inline-block`
+        }
+    })
 }
 catch(error)
 {
